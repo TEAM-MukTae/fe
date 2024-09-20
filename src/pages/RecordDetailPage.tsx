fb72c57f9aed22975f3309880f7a27e03ee09d2e
2NNS-V <msy3824@knu.ac.kr>
2024-09-21 02:23:12 +0900

    feat: 녹음 상세 페이지에 오디오 플레이어 추가

--- src/pages/RecordDetailPage.tsx
+++ src/pages/RecordDetailPage.tsx
@@ -1,9 +1,67 @@
 import { useLocation } from "react-router-dom";
 import { useAudioDetail } from "../hooks/useAudioDetail";
-import Card from "../components/display/Card";
+import { useState } from "react";
+
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 const dummy = {
     data: [
+        {
+            id: 0,
+            script: ["안녕하세요.", "hello", "hi", "안녕"],
+            url: "http://naver.com",
+        },
+        {
+            id: 0,
+            script: ["안녕하세요.", "hello", "hi", "안녕"],
+            url: "http://naver.com",
+        },
+        {
+            id: 0,
+            script: ["안녕하세요.", "hello", "hi", "안녕"],
+            url: "http://naver.com",
+        },
+        {
+            id: 0,
+            script: ["안녕하세요.", "hello", "hi", "안녕"],
+            url: "http://naver.com",
+        },
+        {
+            id: 0,
+            script: ["안녕하세요.", "hello", "hi", "안녕"],
+            url: "http://naver.com",
+        },
+        {
+            id: 0,
+            script: ["안녕하세요.", "hello", "hi", "안녕"],
+            url: "http://naver.com",
+        },
+        {
+            id: 0,
+            script: ["안녕하세요.", "hello", "hi", "안녕"],
+            url: "http://naver.com",
+        },
+        {
+            id: 0,
+            script: ["안녕하세요.", "hello", "hi", "안녕"],
+            url: "http://naver.com",
+        },
+        {
+            id: 0,
+            script: ["안녕하세요.", "hello", "hi", "안녕"],
+            url: "http://naver.com",
+        },
+        {
+            id: 0,
+            script: ["안녕하세요.", "hello", "hi", "안녕"],
+            url: "http://naver.com",
+        },
+        {
+            id: 0,
+            script: ["안녕하세요.", "hello", "hi", "안녕"],
+            url: "http://naver.com",
+        },
         {
             id: 0,
             script: ["안녕하세요.", "hello", "hi", "안녕"],
@@ -22,24 +80,71 @@ function RecordDetailPage() {
     const location = useLocation();
     const { id } = location.state || {};
 
+    const [activeTab, setActiveTab] = useState<string>("script");
+
     const { isLoading, isError, audioDetail } = useAudioDetail(id);
 
     if (isLoading) return <div>Loading...</div>;
     // if (isError) return <div>오류가 발생했습니다.</div>;
 
+    const handleTabClick = (tab: string) => {
+        setActiveTab(tab);
+    };
+
     return (
-        <div className="mt-5">
-            <div>{id}</div>
-            {dummy.data.map(({ id, script, url }: AudioDetail) => (
-                <div key={id} className="mt-5">
-                    {script.map((line, index) => (
-                        <div key={index} className="mt-4">
-                            {line}
-                        </div>
-                    ))}
-                </div>
-            ))}
-        </div>
+        <>
+            <div className="flex justify-around border-b">
+                <button
+                    className={`py-2 px-4 text-center flex-1 font-bold ${
+                        activeTab === "script"
+                            ? "text-black border-b-2 border-black"
+                            : "text-gray-500"
+                    }`}
+                    onClick={() => handleTabClick("script")}
+                >
+                    스크립트
+                </button>
+                <button
+                    className={`py-2 px-4 text-center flex-1 font-bold ${
+                        activeTab === "summary"
+                            ? "text-black border-b-2 border-black"
+                            : "text-gray-500"
+                    }`}
+                    onClick={() => handleTabClick("summary")}
+                >
+                    요약
+                </button>
+            </div>
+
+            <div
+                className="p-4 bg-gray-100 "
+                style={{ paddingBottom: "100px" }}
+            >
+                {activeTab === "script" && (
+                    <>
+                        {dummy.data.map(({ id, script, url }: AudioDetail) => (
+                            <div key={id} className="mt-1">
+                                {script.map((line, index) => (
+                                    <div key={index} className="mt-4">
+                                        {line}
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                    </>
+                )}
+                {activeTab === "summary" && <div>요약 준비중...</div>}
+            </div>
+
+            <div className="fixed bottom-0 left-0 right-0">
+                <AudioPlayer
+                    autoPlay
+                    src="http://example.com/audio.mp3"
+                    onPlay={(e) => console.log("onPlay")}
+                    style={{ width: "100vw" }}
+                />
+            </div>
+        </>
     );
 }
 
