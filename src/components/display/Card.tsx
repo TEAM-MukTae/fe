711ec87f9453241769094259055d677141f47589
2NNS-V <msy3824@knu.ac.kr>
2024-09-21 22:33:30 +0900

    feat: Card에 date 파라미터 추가

--- src/components/display/Card.tsx
+++ src/components/display/Card.tsx
@@ -5,6 +5,7 @@ interface CardProps {
     children: React.ReactNode;
     key?: number;
     keyword?: string[] | string;
+    date?: string;
     isStarred?: boolean;
     isSelected?: boolean;
     onClick?: () => void;
@@ -13,6 +14,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({
     children,
     keyword,
+    date,
     isStarred,
     isSelected,
     onClick,
@@ -27,10 +29,23 @@ const Card: React.FC<CardProps> = ({
             <div className="flex items-center">
                 <div>
                     <p className="font-medium text-gray-900">{children}</p>
+                    <p className="text-sm text-gray-600">{date}</p>
+
                     <p className="text-sm text-gray-500">
                         {Array.isArray(keyword)
-                            ? keyword.join(", ")
-                            : keyword || ""}
+                            ? keyword.map((kw, index) => (
+                                  <button
+                                      key={index}
+                                      className="px-3 py-1 mt-1 mr-1 text-sm text-gray-800 bg-gray-200 rounded-full hover:bg-gray-300"
+                                  >
+                                      {kw}
+                                  </button>
+                              ))
+                            : keyword && (
+                                  <button className="px-3 py-1 mt-1 mr-1 text-sm text-gray-800 bg-gray-200 rounded-full hover:bg-gray-300">
+                                      {keyword}
+                                  </button>
+                              )}
                     </p>
                 </div>
             </div>
